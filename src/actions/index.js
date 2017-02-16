af5d6086fa046268ef6b768a2b81cb3a9ece52ae
janolavm <koffert96>
2017-02-16 13:11:21 +0100

    teste å legge til fil

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
 /**
  * Created by jan on 16.02.2017.
  */
+
+import Firebase from 'firebase';
+
+
+const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
+const API_KEY = '&api_key=dc6zaTOxFJmzC';
+
+const config = {
+    apiKey: "AIzaSyCGT9CHqqN1PtviEyWZ-Nup3Hgt5xkWVIY",
+    authDomain: "pu-gruppe30.firebaseapp.com",
+    databaseURL: "https://pu-gruppe30.firebaseio.com"
+};
+
+Firebase.initializeApp(config);

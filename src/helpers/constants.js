914ffd95eef6e864fdd56e684153d6e656343914
janolavm <koffert96>
2017-02-16 13:32:50 +0100

    auth

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -5,9 +5,6 @@
 import Firebase from 'firebase';
 
 
-const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
-const API_KEY = '&api_key=dc6zaTOxFJmzC';
-
 const config = {
     apiKey: "AIzaSyCGT9CHqqN1PtviEyWZ-Nup3Hgt5xkWVIY",
     authDomain: "pu-gruppe30.firebaseapp.com",

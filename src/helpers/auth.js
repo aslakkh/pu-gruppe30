3f3e2807271dbecc446489e0d8281fbb5a884387
janolavm <koffert96>
2017-02-21 10:51:59 +0100

    auth

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,27 +1,50 @@
-/**
- * Created by jan on 16.02.2017.
- */
-import { ref, firebaseAuth } from '../helpers/constants'
+import { ref, firebaseAuth } from '../config/constants'
+import firebase from 'firebase';
 
 export function auth (email, pw) {
-    return firebaseAuth().createUserWithEmailAndPassword(email, pw)
-        .then(saveUser)
-        .catch((error) => console.log('Oops', error))
+  return firebaseAuth().createUserWithEmailAndPassword(email, pw)
+    .then(saveUser)
+    .catch((error) => console.log('Oops', error))
 }
 
 export function logout () {
-    return firebaseAuth().signOut()
+  return firebaseAuth().signOut()
 }
 
 export function login (email, pw) {
-    return firebaseAuth().signInWithEmailAndPassword(email, pw)
+  return firebaseAuth().signInWithEmailAndPassword(email, pw)
 }
 
 export function saveUser (user) {
-    return ref.child(`users/${user.uid}/info`)
-        .set({
-            email: user.email,
-            uid: user.uid
-        })
-        .then(() => user)
+  return ref.child(`users/${user.uid}/info`)
+    .set({
+      email: user.email,
+      uid: user.uid
+    })
+    .then(() => user)
+}
+
+export function saveCourse (course){
+  const userUid = firebase.auth().currentUser.uid;
+  var usersRef = ref.child('users/'+userUid+"/courses/"+course);
+
+  usersRef.update({
+    active: true
+
+  });
+}
+
+  export function getCourse(){
+    const userUid = firebase.auth().currentUser.uid;
+    this.firebase.database().ref(userUid).once()
+    return firebase.database().ref(userUid).on("value", function(snapshot) {
+      console.log(snapshot.val());
+
+
+    });
+
+
+
+  
+
 }
--- src/reducers.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by jan on 16.02.2017.
- */
-import { combineReducers } from 'redux'
-import { firebaseStateReducer as firebase } from 'react-redux-firebase'
-const rootReducer = combineReducers({
-    firebase
-});
-export default rootReducer

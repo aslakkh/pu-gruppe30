f58c59bf2ba6d6673320c6de1a6f9cff52d919de
Aslak Hauglid <Aslak Hauglid>
2017-02-21 21:05:00 +0100

    Small adjustments to style and validation

--- src/components/protected/AddCourse.js
+++ src/components/protected/AddCourse.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 import { saveCourse,getCourse } from '../../helpers/auth'
-import {FormGroup, FormControl} from 'react-bootstrap'
+import {FormGroup, FormControl, Button, HelpBlock} from 'react-bootstrap'
+import './AddCourse.css'
 
-//Component for adding AddCourse
+/*      Component for adding course
+
+Returns a form and a button.
+Saves form data in firebase upon button click.
+Should be updated to include better form validation. User should probably only be able to add courses that are already in database.
+
 //https://react-bootstrap.github.io/components.html#forms
+
+*/
+
 
 export default class AddCourse extends Component{
     constructor(props){
@@ -25,18 +34,30 @@ export default class AddCourse extends Component{
     //handle change in form value. Sets the state value to form input value
     handleChange(e){
         this.setState({value: e.target.value});
+        
     }
 
-    //function for validating input. Allows anything over 4 characters for now
+    //function for validating input. For now returns error if form has input of length greater than 0 but less than 5, and success for anything of length > 4
     getValidationState(){
         const length = this.state.value.length;
-        return (length > 4) ? 'success' : 'error'; //validates input greater than 4 characters
+        if(length > 4){
+            return 'success';
+        }else if(length > 0){
+            return 'error';
+        }
     }
 
-    //function for handling click on add-button
+    //function for handling click on add-button. Only saves course to database if input is valid.
     handleAdd = (e) => {
 		e.preventDefault(); //prevents default browser behaviour on click, whatever that means
-		saveCourse(this.state.value); //saveCourse(course) adds course to user in Firebase
+
+        if(this.getValidationState() == 'success'){
+            saveCourse(this.state.value); //saveCourse(course) adds course to currently logged in user in Firebase
+            this.setState({value: ''}); //resets value to empty string
+        }
+        else{
+            console.log("Error: Tried submitting invalid value: " + this.state.value);
+        }
 		//getCourse();
 	}
 
@@ -46,16 +67,19 @@ export default class AddCourse extends Component{
             <div className="AddCourse">
                 <div className="Form">
                     <form>
-                        <FormGroup controlId="1" bsSize="small" style={{width: "50%"}}>
+                        <FormGroup controlId="addCourseForm" validationState={this.getValidationState()}>
                             <FormControl 
-                            id="1" value={this.state.value} 
+                            id="addCourseForm" value={this.state.value} 
                             type="text" 
                             placeholder="Course name"
                             onChange={this.handleChange}
+                            style={{width: "400px"}}
                             />
+                            <FormControl.Feedback/>
+                            <HelpBlock>Enter a course (must have at least 5 characters)</HelpBlock>
                         </FormGroup>
                     </form>
-                    <button className="btn btn-primary btn-lg" onClick={this.handleAdd}>legg til emne</button>
+                    <Button bsStyle="primary" bsSize="large" onClick={this.handleAdd}>legg til emne</Button>
                 </div>
             </div>
         )

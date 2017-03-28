2def79fed8a9b291bdf036c48170916c34430737
Aslak Hauglid <=>
2017-03-28 17:20:20 +0200

    complete refactoring of AddCourse-code (now only input and button)

--- src/components/Dashboard/AddCourse.js
+++ src/components/Dashboard/AddCourse.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { saveCourse } from '../../helpers/auth'
-import {FormGroup, FormControl, Button, HelpBlock} from 'react-bootstrap'
+import {FormGroup, FormControl, Button, HelpBlock, ListGroup, ListGroupItem} from 'react-bootstrap'
+import firebase from 'firebase'
+import AddCourseModal from './AddCourseModal.js'
 import './AddCourse.css'
 
 /*      Component for adding course
@@ -25,21 +27,26 @@ export default class AddCourse extends Component{
         this.handleAdd = this.handleAdd.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        //console.log(this.props.courses);
+        //var coursesArray = Object.keys(this.props.courses).map(key => key);
         this.state = {
-            value: ''
+            value: '',
         }
     }
 
     //get intialstate of form component. 
     getInitialState(){
         return {
-            value: ''
+            value: '',
+            
         }
     }
+
 
     //handle change in form value. Sets the state value to form input value
     handleChange(e){
         this.setState({value: e.target.value.toUpperCase()});
+        
         
     }
 
@@ -78,27 +85,42 @@ export default class AddCourse extends Component{
 		//getCourse();
 	}
 
+    handleAddToMyCourses(course,e){
+        e.preventDefault();
+        //add to users courses in firebase
+        saveCourse(course);
+        this.setState({
+                    displayFeedbackMessage: true,
+                    feedbackMessage: course + " has been added to your courses!",
+                    feedbackMessagePositive: true,
+        });
+    }
+
+
     //Renders a form and a button for submission
     render(){
         return(
-            <div className="FormWrapper" >
+            <div>
                     
-                        <FormGroup controlId="addCourseForm" validationState={this.getValidationState()} >
-                            <FormControl 
-                            id="addCourseForm" 
-                            componentClass='input'
-                            value={this.state.value}
-                            className="InputForm" 
-                            type="text" 
-                            placeholder="Course name"
-                            onChange={this.handleChange}
-                            onKeyPress={this.handleKeyPress}
-                            
-                            />
-                            <FormControl.Feedback/>
-                            <HelpBlock>Enter course code (up to three letters followed by four digits)</HelpBlock>
-                        </FormGroup>
-                    <Button type="submit" bsStyle="primary" bsSize="large" onClick={this.handleAdd} style={{marginLeft: "20px"}}>Add</Button>
+                <div className="AddCourseForm">
+                            <FormGroup controlId="addCourseForm" validationState={this.getValidationState()} >
+                                <FormControl 
+                                id="addCourseForm" 
+                                componentClass='input'
+                                value={this.state.value}
+                                className="InputForm" 
+                                type="text" 
+                                placeholder="Course name"
+                                onChange={this.handleChange}
+                                onKeyPress={this.handleKeyPress}
+                                
+                                />
+                                <FormControl.Feedback/>
+                                <HelpBlock>Enter course code (up to three letters followed by four digits)</HelpBlock>
+                            </FormGroup>
+                </div>
+                <Button type="submit" bsStyle="primary" bsSize="large" onClick={this.handleAdd} style={{marginLeft: "20px"}}>Add</Button>
+
             </div>
         )
     } 

05e6be3028f7e5a7dba042a12e2e46a64821c422
Aslak Hauglid <=>
2017-04-18 19:29:13 +0200

    snapshot test for ManageCourses

---
+++ src/components/Dashboard/ManageCoursesStyles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const styles = {
+    List:{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+
+    CourseName:{
+        flexBasis: '90%',
+    },
+
+    Button: {
+        marginLeft: '5px',
+        marginRight: '5px'
+    },
+
+    ModalHeader: {
+        backgroundColor: 'red',
+    },
+
+    ModalTitle: {
+        color: 'white',
+    },
+
+    FormWrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        marginTop: '10px',
+    }
+
+}
